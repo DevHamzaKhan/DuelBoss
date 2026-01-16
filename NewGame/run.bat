0f22428275e77d5d11a34f89d7dbcbdc6b505ba9
Alec Li <yunshen08@gmail.com>
2026-01-16 08:38:38 -0500

    fdsa

--- NewGame/run.bat
+++ NewGame/run.bat
@@ -1,11 +1,4 @@
-@echo off
-cd /d "%~dp0"
-javac -d . util\*.java entity\*.java particle\*.java enemy\*.java ability\*.java manager\*.java ui\*.java core\*.java
-if %ERRORLEVEL% NEQ 0 (
-    echo Compilation failed!
-    pause
-    exit /b %ERRORLEVEL%
-)
-echo Compilation successful! Starting game...
-java -cp . core.Game
+javac util\*.java entity\*.java particle\*.java enemy\*.java ability\*.java manager\*.java ui\*.java core\*.java
+pause
+java core.Game
 pause
--- NewGame/run.sh
+++ NewGame/run.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-javac -d . util/*.java entity/*.java particle/*.java enemy/*.java ability/*.java manager/*.java ui/*.java core/*.java
-if [ $? -ne 0 ]; then
-    echo "Compilation failed!"
-    exit 1
-fi
-echo "Compilation successful! Starting game..."
-java -cp . core.Game
+javac -d . util/*.java entity/*.java particle/*.java enemy/*.java ability/*.java manager/*.java ui/*.java core/*.java&&java -cp . core.Game
